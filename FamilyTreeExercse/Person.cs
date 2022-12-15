39ad2b7290f91c5852fdd1db0526ab09b51fe322
GET Academy <terje@kolderup.net>
2022-12-15 09:30:19 +0100

    Initial commit

---
+++ FamilyTreeExercse/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace FamilyTreeExercse
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? BirthYear { get; set; }
+        public int? DeathYear { get; set; }
+        public Person Father { get; set; }
+        public Person Mother { get; set; }
+
+        public Person(string firstName, int? birthYear = null, string lastName = null, int? deathYear = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+            DeathYear = deathYear;
+        }
+
+        public Person(string firstName, int birthYear, int deathYear)
+            :this(firstName, birthYear, null, deathYear)
+        {
+        }
+    }
+}
---
+++ FamilyTreeExercse/Program.cs
@@ -0,0 +1,19 @@
+﻿using FamilyTreeExercse;
+
+var sverreMagnus = new Person("Sverre Magnus", 2005);
+var ingridAlexandra = new Person("Ingrid Alexandra", 2004);
+var haakon = new Person("Haakon Magnus", 1973);
+var metteMarit = new Person("Mette-Marit", 1973);
+var marius = new Person("Marius", 1997, "Borg Høiby");
+var harald = new Person("Harald", 1937);
+var sonja = new Person("Sonja", 1937);
+var olav = new Person("Olav", 1903, 1991);
+
+sverreMagnus.Father = haakon;
+sverreMagnus.Mother = metteMarit;
+ingridAlexandra.Father = haakon;
+ingridAlexandra.Mother = metteMarit;
+marius.Mother = metteMarit;
+haakon.Father = harald;
+haakon.Mother = sonja;
+harald.Father = olav;
